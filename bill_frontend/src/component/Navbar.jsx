61f929149f742a9ada2b2a020c915a739701534e
DebaPrasadbehura424 <DebaPrasadbehura424@gmail.com>
2025-08-14 17:15:59 +0530

    ok

--- bill_frontend/src/component/Navbar.jsx
+++ bill_frontend/src/component/Navbar.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
-    <nav className="bg-white shadow-md fixed w-full top-0 left-0 z-50">
+    <nav className="bg-[#0A0A0A] shadow-md fixed w-full top-0 left-0 z-50">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold text-blue-600">
+        <Link to="/" className="text-xl font-bold text-[#FAFAFA]">
           City Compliance
         </Link>
         <div className="space-x-6">
-          <Link to="/heatmap" className="hover:text-blue-600">
+          <Link to="/heatmap" className="hover:text-gray-300 text-[#FAFAFA]">
             Public Map
           </Link>
-          <Link to="/about" className="hover:text-blue-600">
+          <Link to="/about" className="hover:text-gray-300 text-[#FAFAFA]">
             About
           </Link>
-          <Link to="/signup" className="hover:text-blue-600">
+          <Link to="/signup" className="hover:text-gray-300 text-[#FAFAFA]">
             Sign Up
           </Link>
         </div>
